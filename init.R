6607a6acbc91864f8ccc522de1add23336a87c8d
Crist H <cristianvoice@hotmail.com>
2021-12-09 01:22:41 -0400

    change of buildpack

--- init.R
+++ init.R
@@ -1,14 +1,13 @@
 my_packages = c("dplyr", "DT","lubridate","tidyr","htmltools","shinythemes",
-                "ggplot2", "remotes", "Rcpp")
+                "ggplot2", "Rcpp")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
-    install.packages(p, dependencies = TRUE)
+    install.packages(p)
   }  else {
     cat(paste("Skipping already installed package:", p, "\n"))
   }
 }
-remotes::install_github('talgalili/installr')
-installr::install.Rtools()
+
 
 invisible(sapply(my_packages, install_if_missing))
