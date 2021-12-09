868e21ba34bb044ee6533c499bd20b12f46692d7
Crist H <cristianvoice@hotmail.com>
2021-12-08 23:33:46 -0400

    modified init

--- init.R
+++ init.R
@@ -3,7 +3,9 @@ my_packages = c("dplyr", "DT","lubridate","tidyr","htmltools","shinythemes",
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
-    install.packages(p)
+    install.packages(p, dependencies = TRUE)
+  }  else {
+    cat(paste("Skipping already installed package:", p, "\n"))
   }
 }
 
